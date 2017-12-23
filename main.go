7a206d5dc62d8571d8bd5e39c228d0a584883ecb
naofumi-fujii <naofumi-fujii@users.noreply.github.com>
2017-12-23 10:15:40 +0900

    Update main.go

--- main.go
+++ main.go
@@ -10,16 +10,13 @@ import (
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
-		// fmt.Println(scanner.Text()) // Println will add back the final '\n'
-
-		// remoteURL := "ssh://git@github.com/naofumi-fujii/my-sample-rails.git"
-
 		remoteURL := scanner.Text()
 		splitted := strings.Split(remoteURL, "/")
 		repoNameWithGit := splitted[len(splitted)-1]
 		repoName := strings.Replace(repoNameWithGit, ".git", "", -1)
 		userName := splitted[len(splitted)-2]
-		result := []string{repoName, userName}
+		hostName := splitted[len(splitted)-3]
+		result := []string{hostName, userName, repoName}
 		fmt.Println(strings.Join(result, "/"))
 	}
 	if err := scanner.Err(); err != nil {
